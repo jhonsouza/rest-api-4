bf3e59b49c894661d0b4cf843f33b80f9edeec4e
Daniel Oliveira <danieliver2011@gmail.com>
2021-04-03 16:32:33 -0300

    Add env variables for database

--- server/infra/database.js
+++ server/infra/database.js
@@ -1,10 +1,10 @@
 const pgp = require('pg-promise')();
 const db = pgp({
-	user: 'postgres',
-	password: '',
-	host: 'localhost',
-	port: 5432,
-	database: 'blog'
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	database: process.env.DN_NAME
 });
 
 module.exports = db;
